1c8f535bc5da9e4721628576a8ec4025260fc1ae
Krishnendu0016 <16kdas@gmail.com>
2024-03-11 21:36:49 +0530

    updated

--- src/com/CustomInput.js
+++ src/com/CustomInput.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from "react";
 
-const CustomInput = () => {
+const CustomInput = (props) => {
+  const { type, label, i_id, i_class, name, val, onChng, onBlr } = props;
   return (
-    <div>CustomInput</div>
-  )
-}
+    <div className="form-floating mt-3">
+      <input
+        type={type}
+        className={`form-control ${i_class}`}
+        id={i_id}
+        placeholder={label}
+        name={name}
+        value={val}
+        onChange={onChng}
+        onBlur={onBlr}
+      />
+      <label htmlFor={label}>{label}</label>
+    </div>
+  );
+};
 
-export default CustomInput
+export default CustomInput;
